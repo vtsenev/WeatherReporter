88acf93d652b00dae3a23c1515823e45b695e849
Vladimir Tsenev <vladimir.tsenev@mentormate.com>
2012-06-13 17:29:44 +0300

    Now displaying the city on a map view

---
+++ WeatherReporter/WeatherPeriodsViewController.h
@@ -0,0 +1,13 @@
+//
+//  WeatherPeriodsViewController.h
+//  WeatherReporter
+//
+//  Created by Vladimir Tsenev on 6/13/12.
+//  Copyright (c) 2012 MentorMate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WeatherPeriodsViewController : UITableViewController
+
+@end
