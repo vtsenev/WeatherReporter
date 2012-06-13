1f4e09f3e1cf1371aa51ff6575d42ca6eb83c285
Victor Hristoskov <VHristoskov@Victors-Mac-mini.local>
2012-06-13 15:47:34 +0300

    added delegate for date picker view controller

---
+++ WeatherReporter/DatePickerControllerDelegate.h
@@ -0,0 +1,13 @@
+//
+//  DatePickerControllerDelegate.h
+//  WeatherReporter
+//
+//  Created by Victor Hristoskov on 6/13/12.
+//  Copyright (c) 2012 MentorMate. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol DatePickerControllerDelegate <NSObject>
+
+@end
