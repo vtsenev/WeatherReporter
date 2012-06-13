194f568d0558d0f4ac40159b66cfc070ced8e2e6
Vladimir Tsenev <vladimir.tsenev@mentormate.com>
2012-06-13 20:42:56 +0300

    Added Detailed Forecast for a weather period and weather periods table view header view

---
+++ WeatherReporter/DetailedForecastViewController.h
@@ -0,0 +1,21 @@
+//
+//  DetailedForecastViewController.h
+//  WeatherReporter
+//
+//  Created by Vladimir Tsenev on 6/13/12.
+//  Copyright (c) 2012 MentorMate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "WeatherPeriod.h"
+
+@interface DetailedForecastViewController : UIViewController
+@property (retain, nonatomic) IBOutlet UILabel *locationField;
+@property (retain, nonatomic) IBOutlet UILabel *dateField;
+@property (retain, nonatomic) IBOutlet UILabel *lowTempField;
+@property (retain, nonatomic) IBOutlet UILabel *highTempField;
+@property (retain, nonatomic) IBOutlet UITextView *detailedForecastView;
+@property (retain, nonatomic) IBOutlet UIImageView *weatherImageView;
+@property (retain, nonatomic) WeatherPeriod *weatherPeriod;
+
+@end
